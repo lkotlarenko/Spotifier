b886d7ded9cb85ea7e84e3d547bf100fecf38b8d
lkotlarenko <lkotlarenko@protonmail.com>
2022-02-01 18:56:36 -0300

    login redirect working

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -53,6 +53,13 @@ export default NextAuth({
         }
 
         return await refreshAccessToken(token);
+      },
+
+      async session({ session, token }) {
+          session.user.accessToken = token.accessToken;
+          session.user.refreshToken = token.refreshToken;
+          session.user.username = token.username;
+          return session;
       }
   }
 });
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,28 @@
-const Login = () => {
+import { getProviders, signIn } from "next-auth/react";
+
+const Login = ({ providers }) => {
   return (
-    <div>
+    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
+        <img className="w-52 mb-5" src="https://github.com/lkotlarenko/Spotifier/blob/main/public/spotify_logo.png" alt="Spotify Logo" />
+        {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+                <button
+                    className="bg-[#18D860] text-white p-5 rounded-lg"
+                    onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+                >Login with {provider.name}</button>
+            </div>
+        ))}
         <h2>Login page :P</h2>
     </div>
   )
 };
 
 export default Login;
+export async function getServerSideProps() {
+    const providers = await getProviders();
+    return {
+        props: {
+            providers,
+        },
+    };
+}
