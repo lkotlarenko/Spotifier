01a6b4e0c63c0c54c19c2b1cb14bdd53eb9299f9
lkotlarenko <lkotlarenko@protonmail.com>
2022-02-02 14:59:28 -0300

    playSong working :star:

--- components/Track.jsx
+++ components/Track.jsx
@@ -1,11 +1,27 @@
-
-import useSpotify from '../hooks/useSpotify';
-import { millisToMinutesAndSeconds } from '../lib/time';
+import useSpotify from '../hooks/useSpotify'
+import { millisToMinutesAndSeconds } from '../lib/time'
+import { useRecoilState } from 'recoil'
+import { currentTrackIdState, isPlayingState } from '../atoms/songAtom'
 
 const Track = ({ order, track }) => {
-  const spotifyApi = useSpotify();
+  const spotifyApi = useSpotify()
+  const [currentTrackId, setCurrentTrackId] =
+    useRecoilState(currentTrackIdState)
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState)
+
+  const playSong = () => {
+    setCurrentTrackId(track.track.id);
+    setIsPlaying(true);
+    spotifyApi.play({
+      uris: [track.track.uri],
+    })
+  }
+
   return (
-    <div className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer">
+    <div
+      className="grid cursor-pointer grid-cols-2 rounded-lg py-4 px-5 text-gray-500 hover:bg-gray-900"
+      onClick={playSong}
+    >
       <div className="flex items-center space-x-4">
         <p>{order + 1}</p>
         <img
@@ -14,17 +30,17 @@ const Track = ({ order, track }) => {
           alt={track.name}
         />
         <div>
-          <p className="w-36 lg:w-64 truncate text-white">{track.track.name}</p>
+          <p className="w-36 truncate text-white lg:w-64">{track.track.name}</p>
           <p className="w-40">{track.track.artists[0].name}</p>
         </div>
       </div>
 
-        <div className="flex items-center justify-between ml-auto md:ml-0">
-          <p className="w-40 hidden md:inline">{track.track.album.name}</p>
-          <p>{millisToMinutesAndSeconds(track.track.duration_ms)}</p>
-        </div>
+      <div className="ml-auto flex items-center justify-between md:ml-0">
+        <p className="hidden w-40 md:inline">{track.track.album.name}</p>
+        <p>{millisToMinutesAndSeconds(track.track.duration_ms)}</p>
       </div>
-  );
+    </div>
+  )
 }
 
 export default Track
